5db4132536531af050b67da8f050cc4bed8d29ef
agalecki <agalecki@umich.edu>
2022-01-02 14:16:46 -0500

    Update Dropbox.path.R

--- R/Dropbox.path.R
+++ R/Dropbox.path.R
@@ -1,14 +1,25 @@
 Dropbox.path <- function () {
 ## see https://stackoverflow.com/questions/35985167/determining-the-dropbox-path-in-r
 # By default returns path to Dropbox
-appath <-  file.path( Sys.getenv(x = "LOCALAPPDATA"), "Dropbox", "info.json")
-if (length(appath) == 0) appath <- file.path( Sys.getenv(x = "APPDATA"), "Dropbox", "info.json")
-file_content <- jsonlite::fromJSON(txt= appath)$personal
-dropbox_pth <- file.path(file_content$path)
 
-if (dir.exists(dropbox_pth)){
-     assign(".Dropbox.path", dropbox_pth, envir = .GlobalEnv)
-     message(".Dropbox.path: ", .Dropbox.path)
+appath <-  file.path(Sys.getenv(x = "LOCALAPPDATA"), "Dropbox", "info.json")
+if (length(appath) == 0) appath <- file.path( Sys.getenv(x = "APPDATA"), "Dropbox", "info.json")
+file_content <- jsonlite::fromJSON(txt= appath)
+nms <- names(file_content)  # personal | business
+
+dropbox_pth <- sapply(nms, FUN= function(x){
+     ex <-file_content[[x]]
+     file.path(ex$path)
+     })
+if (length(dropbox_pth) == 0) then {
+   message ("Dropbox not found")    
+     } else {
+assign(".Dropbox.path", dropbox_pth, envir = .GlobalEnv)
+message(".Dropbox.path: ", .Dropbox.path)
 }
 invisible(dropbox_pth)
 }
+Dropbox.path()   # Path to ersonal dropbox by default
+
+
+.Dropbox.path
