e1843d006d9df78a598446c30e943655466080ab
agalecki <agalecki@umich.edu>
2022-10-21 12:35:21 -0400

    v 0.0.6

---
+++ R/proteins21.R
@@ -0,0 +1,12 @@
+#' @title Data with expression of 21 proteinss 
+#'
+#' @description ??.
+#'
+#' @format A data frame with x rows and ? variables:
+#' \describe{
+#'   \item{group}{Label1}
+#'   \item{score}{Label2}
+#' }
+#' @source <https://www.github.com/agalecki-work/penAFTutils>
+"proteins21"
+NULL
