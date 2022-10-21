8b68610277f85f8f68df502885d60f0da546fcf1
agalecki <agalecki@umich.edu>
2022-10-21 12:30:46 -0400

    Update ex1-glmnet.R

--- examples/ex1-glmnet.R
+++ examples/ex1-glmnet.R
@@ -1,4 +1,4 @@
-
+### glmnet_fit1
 myglance(glmnet_fit1)  # coef by default
 sel <- c(2,10)
 
@@ -6,24 +6,27 @@ tidy(glmnet_fit1) %>% filter(step %in% sel) %>% arrange(step)
 mytidy(glmnet_fit1) %>% filter(step %in% sel)
 mytidy(glmnet_fit1) %>% filter(step %in% sel) %>% unnest(coefs)
 
-
+### glmnet_fit3
+sel <- 5
 myglance(glmnet_fit3)
-sel <- 10
 tidy(glmnet_fit3) %>% filter(step == sel) %>% arrange(step, class)
-
 mytidy(glmnet_fit3) %>% filter(step == sel)
 mytidy(glmnet_fit3) %>% filter(step == sel) %>% 
-  unnest(coefs)
+   unnest(coefs)
 
-
+### glmnet_fit3a
+sel <- 3
 myglance(glmnet_fit3a)
-tidy(glmnet_fit3a) %>% arrange(step)
-tidy(glmnet_fit3a) %>% arrange(step) %>% filter(step == 3)
-mytidy(glmnet_fit3a)
-mytidy(glmnet_fit3a) %>% filter(step == 3) 
+tidy(glmnet_fit3a) %>% filter(step == sel) %>% arrange(step, class)
+mytidy(glmnet_fit3a) %>% filter(step == sel) 
+mytidy(glmnet_fit3a) %>% filter(step == sel) %>% 
+   unnest(coefs)
 
 
 myglance(glmnet_fit_cox)
+
+tidy(glmnet_fit_cox)
+
 mytidy(glmnet_fit_cox)
 
 
