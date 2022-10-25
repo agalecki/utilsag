0f9e4e78320fb1106d61d15471d3ccb2d45b99f2
agalecki <agalecki@umich.edu>
2022-10-25 15:30:54 -0400

    Update glmnetUtils-cva-mytidy.R

--- R/glmnetUtils-cva-mytidy.R
+++ R/glmnetUtils-cva-mytidy.R
@@ -2,13 +2,13 @@
 #' @method myglance cva.glmnet
 #' @export
 myglance.cva.glmnet <- function(x){
- callx <- call(x)
+ xcall <- x$call
  mod1 <- x$modlist[[1]]
  modfit1 <- mod1$glmnet.fit
  ret <- tibble::tibble(
     n_alpha = length(x$alpha),
     n_folds = x$nfolds,
-    family  = callx$family,
+    family  = xcall$family,
     n_colx  = modfit1$dim[1]
     )
 return(ret)
