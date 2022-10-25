29a3dad8a13c1fc7768e07da24cbd3e70aa6ff23
agalecki <agalecki@umich.edu>
2022-10-24 21:12:33 -0400

    Update glmnet-mytidy.R

--- R/glmnet-mytidy.R
+++ R/glmnet-mytidy.R
@@ -55,8 +55,8 @@ return(dplyr::bind_cols(ret0,ret1))
 #' 
 #' @importFrom tibble tibble
 #' @importFrom magrittr `%>%`
-#' @importFrom broom glance
-#' @importFrom broom tidy
+#' @importFrom broom glance tidy
+#' @importGrom(dplyr select nest group_by arrange
 #' @method mytidy glmnet
 #' @export
 
