9878840eb6b725c07ca32487f7f08b6e26b24a5a
agalecki <agalecki@umich.edu>
2022-10-26 16:28:37 -0400

    Update glmnetUtils-cva-mytidy.R

--- R/glmnetUtils-cva-mytidy.R
+++ R/glmnetUtils-cva-mytidy.R
@@ -39,7 +39,7 @@ mytidy.cva.glmnet <- function(x, return_zeros = FALSE, ...){
      print(colnames(tbl1))
      print(colnames(tbl2))
     }
-    tbl2 <- tbl2 %>% group_by(alpha_idx) %>% nest(step = c(lambda))
+    tbl2 <- tbl2 %>% group_by(alpha_idx) %>% nest(steps = c(step, lambda))
     if (i ==1)  print(colnames(tbl2))
     left_join(tbl1, tbl2, by = "alpha_idx")
     }
