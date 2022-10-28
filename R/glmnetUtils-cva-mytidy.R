ab2a3c89ca6a6caab1b135b0264e8036297132ba
agalecki <agalecki@umich.edu>
2022-10-28 09:41:38 -0400

    Update glmnetUtils-cva-mytidy.R

--- R/glmnetUtils-cva-mytidy.R
+++ R/glmnetUtils-cva-mytidy.R
@@ -39,7 +39,7 @@ mytidy.cva.glmnet <- function(x, return_zeros = FALSE, unnest = character(1), ..
     tbl3 <- tibble(a_idx = i, mytidy(fiti, return_zeros = return_zeros, unnest = character(1), ...))
     
     ret1 <- left_join(tbl1, tbl2, by = "a_idx") 
-    ret  <- bind_cols(ret1, tbl3)
+    ret  <- left_rows(ret1, tbl3, by = c("a_idx", "step"))
     ret
  }
  ret <- alphas %>% map_dfr(funi)          
