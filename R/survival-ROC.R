dff844e77d4f5eeac80dcb3672048938c98af00b
agalecki <agalecki@umich.edu>
2022-12-14 17:29:03 -0500

    R\survival-ROC.R

--- R/survival-ROC.R
+++ R/survival-ROC.R
@@ -5,18 +5,8 @@
 ## Define a helper function to evaluate at various t
 # Not @export
 
-survivalROC_helper <- function(t, data, marker, time = time, status = status) {
-    mm <- as.character(substitute(marker))
-    tt <- as.character(substitute(time))
-    ss <- as.character(substitute(status))
-    cnms <- colnames(data)
-    exit <- FALSE
-    if (!any(cnms == mm)) exit <- TRUE
-    if (!any(cnms == tt)) exit <- TRUE
-    if (!any(cnms == ss)) exit <- TRUE
-
-    if (exit) stop("One(or more) variables:", mm, tt, ss, " not found in ", data) 
-    
+survivalROC_helper <- function(t, data, mm, tt, ss) {
+     
     survivalROC::survivalROC(Stime = data[[tt]],
                 status       = data[[ss]],
                 marker       = data[[mm]],
@@ -25,16 +15,27 @@ survivalROC_helper <- function(t, data, marker, time = time, status = status) {
                 span = 0.25 * nrow(data)^(-0.20))
 }
 
-# dtx <-survivalROC_helper(15, dt, disp)    
-#names(dtx)
 
 
-# `create_survivalROC_data()` function 
+#' `create_survivalROC_data()` function 
 
-create_survivalROC_data <- function(tvec, cmarker){ 
+#' @export
+create_survivalROC_data <- function(tvec, data, marker, time = time, status=status){
+
     # Ex. cmarker = "lp_M2"
+       mm <- as.character(substitute(marker))
+        tt <- as.character(substitute(time))
+        ss <- as.character(substitute(status))
+        cnms <- colnames(data)
+        exit <- FALSE
+        if (!any(cnms == mm)) exit <- TRUE
+        if (!any(cnms == tt)) exit <- TRUE
+        if (!any(cnms == ss)) exit <- TRUE
+    
+        if (exit) stop("One(or more) variables:", mm, tt, ss, " not found in ", data) 
+
     tibble(t = tvec) %>%
-    mutate(survivalROC = map(t, survivalROC_helper, markr = cmarker),
+    mutate(survivalROC = map(t, survivalROC_helper, data = data, mm = mm, tt=tt, ss=ss),
            ## Extract scalar AUC
            auc = map_dbl(survivalROC, magrittr::extract2, "AUC"),
            ## Put cut off dependent values in a data_frame
@@ -49,7 +50,7 @@ create_survivalROC_data <- function(tvec, cmarker){
 
 # Auxiliary `plot_timedep_ROC()` function to Plot Time-dependent ROC is defined
 
-
+#' @export
 plot_timedep_ROC <- function(surv_ROC_data){
  surv_ROC_data %>%
     ggplot(mapping = aes(x = FP, y = TP)) +
